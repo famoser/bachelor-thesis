b0d3f0503e3ccac1aace4e8af9f6b989e4661ef9
Florian Moser <products@famoser.ch>
2018-07-11 17:51:04 +0200

    commit that skubala

---
+++ experiments/4_statistical_analysis/create_plot_db.py
@@ -0,0 +1,123 @@
+import sqlite3
+import os.path
+from python_libs.config import StaticConfig
+from python_libs.har_analyzer import HarAnalyzer
+
+config = StaticConfig()
+analyzer = HarAnalyzer(config.captures_dir, '.json')
+
+START_AGGREGATION = 1
+LAST_AGGREGATION = 10
+
+# get sqlite connection
+db_file_name = config.captures_dir + "/data.sqlite"
+if os.path.exists(db_file_name):
+    os.remove(db_file_name)
+connection = sqlite3.connect(db_file_name)
+
+# create tables
+connection.execute(
+    "CREATE TABLE captures ("
+    "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+    "movie_id INTEGER, "
+    "bitrate INTEGER, "
+    "created_at TEXT)")
+
+connection.execute(
+    "CREATE TABLE packets ("
+    "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+    "capture_id INTEGER, "
+    "body_size INTEGER, "
+    "aggregation INTEGER)")
+
+connection.commit()
+
+for aggregation in range(START_AGGREGATION, LAST_AGGREGATION + 1):
+    connection.execute(
+        "CREATE TABLE packets_" + str(aggregation) + "_continuous " +
+        "("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "capture_id INTEGER, "
+        "body_size INTEGER)")
+
+burned = {}
+
+# create lookup like lookup[movie_id][bandwidth] = video_size
+for file_name in analyzer.get_file_names():
+    # analyze file name
+    splits = file_name.split("_")
+    movie_id = int(splits[0])
+    bitrate = int(splits[1])
+    created_at = splits[2] + ":" + splits[3] + ":" + splits[4].replace(".json", "")
+
+    # do not insert two times
+    if movie_id in burned and bitrate in burned[movie_id]:
+        continue
+
+    # add to burned to avoid double insertion
+    if movie_id not in burned:
+        burned[movie_id] = []
+    burned[movie_id].append(bitrate)
+
+    # insert capture info & get id
+    cursor = connection.cursor()
+    cursor.execute(
+        "INSERT INTO captures (movie_id, bitrate, created_at) VALUES (?, ?, ?)",
+        [movie_id, bitrate, created_at])
+    capture_id = cursor.lastrowid
+    connection.commit()
+
+    # transform har entries to sqlite entry
+    sizes = analyzer.get_ordered_sizes(analyzer.get_har_entries_dict()[file_name])
+
+    # aggregate
+    for aggregation in range(START_AGGREGATION, LAST_AGGREGATION + 1):
+
+        # all possible aggregations
+        insert_array = []
+        for i in range(0, len(sizes)):
+            if i + aggregation > len(sizes):
+                continue
+
+            size = 0
+            for j in range(0, aggregation):
+                size += sizes[i + j]
+
+            insert_array.append([capture_id, size, aggregation])
+
+        # insert har entries to db
+        connection.executemany(
+            "INSERT INTO packets "
+            "(capture_id, body_size, aggregation) "
+            "VALUES "
+            "(?, ?, ?)",
+            insert_array)
+        connection.commit()
+
+        # continuous aggregation
+        current_aggregation = 0
+        current_value = 0
+        insert_array = []
+        for size in sizes:
+            current_aggregation += 1
+            current_value += size
+            if current_aggregation == aggregation:
+                insert_array.append([capture_id, current_value])
+                current_aggregation = 0
+                current_value = 0
+
+        # insert har entries to db
+        connection.executemany(
+            "INSERT INTO packets_" + str(aggregation) + "_continuous " +
+            "(capture_id, body_size) "
+            "VALUES "
+            "(?, ?)",
+            insert_array)
+        connection.commit()
+
+        print("done for " + str(movie_id) + " at bitrate " + str(bitrate) + " (" + str(aggregation) + ")")
+
+for aggregation in range(START_AGGREGATION, LAST_AGGREGATION + 1):
+    connection.execute(
+        "CREATE INDEX packets_" + str(aggregation) + "_continuous_index ON packets_" + str(
+            aggregation) + "_continuous (body_size)")
---
+++ experiments/4_statistical_analysis/python_libs/bandwidth_manipulator.py
@@ -0,0 +1,70 @@
+# this script is able to enforce machine-wide bandwith limitations using tc
+
+import pickle
+import os
+import json
+import time
+import requests as request
+import datetime
+import psutil
+import subprocess
+import time
+import json
+
+from .config import StaticConfig
+
+static_config = StaticConfig()
+
+
+class BandwidthManipulator:
+    __log_file = None
+    __disable = None
+
+    def __init__(self, disable = False):
+        self.__disable = disable
+
+    def __enter__(self):
+        self.__log_file = open(static_config.log_dir + "/" + "tcset.log", "w")
+        self.ensure_sudo()
+        self.clear_rate()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.clear_rate()
+        self.__log_file.close()
+
+    @staticmethod
+    def ensure_sudo():
+        os.system("sudo ls > /dev/null")  # force user to put in password
+
+    def set_rate(self, rate: str = "1M"):
+        """
+        limit device from configuration to specified rate for incoming connections
+        :param rate:
+        """
+
+        if self.__disable:
+            return
+
+        subprocess.Popen(
+            "sudo"
+            " tcset --device " + static_config.network_device +  # set device
+            " --direction incoming" +  # modify only incoming connections
+            " --rate " + rate +  # set the target rate
+            " --overwrite",  # overwrite existing configuration
+            shell=True, universal_newlines=True, stdout=self.__log_file, stderr=self.__log_file
+        )
+
+    def clear_rate(self):
+        """
+        clear any limitations set for the configured device
+        """
+        if self.__disable:
+            return
+
+        subprocess.Popen(
+            "sudo"
+            " tcdel --device " + static_config.network_device +  # set device
+            " --all",  # clear all
+            shell=True, universal_newlines=True, stdout=self.__log_file, stderr=self.__log_file
+        )
