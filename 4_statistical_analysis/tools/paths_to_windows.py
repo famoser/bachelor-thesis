297ba0b4ec82e7ac23c578abe85fce49a81eaeb1
Florian Moser <products@famoser.ch>
2018-07-12 16:48:52 +0200

    removing duplicated folder

--- 4_statistical_analysis/tools/paths_to_windows.py
+++
@@ -1,9 +0,0 @@
-import os
-pathiter = (os.path.join(root, filename)
-    for root, _, filenames in os.walk(".")
-    for filename in filenames
-)
-for path in pathiter:
-    newname =  path.replace(':', '_')
-    if newname != path:
-        os.rename(path,newname)
